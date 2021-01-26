97b6947fff1369759f7eaab9268eb2547ddfadde
piyush sharma <sharmapiyush1001@gmail.com>
2021-01-26 15:19:21 +0530

    initial commit

--- app/index.js
+++ app/index.js
@@ -26,7 +26,7 @@ app.use(express.static(publicDirectoryPath))
 app.get('', (req,res) => {
     res.render('index', {
         name: "Piyush Sharma",
-        title: "Weather App"
+        title: "todo app"
     })
 })
 
